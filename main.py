778dc0dae1cf75bb7fc4dab278ea869929fff3bd
David-Bonck <david.m.bonck@email.com>
2026-04-24 15:27:41 -0500

    Organized code into folders. New main.py file created. The .csv file is now in the git ignore.

---
+++ main.py
@@ -0,0 +1,15 @@
+# entry point for the application
+
+# import methods 
+
+
+#intialize methods
+
+
+#start application function
+
+def main():
+    print ("hello world")
+
+if __name__ == "__main__":
+    main()
